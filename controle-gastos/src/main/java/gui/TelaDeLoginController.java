6fa9c7efd6daa2cbec38376ce35fdd41be1d4597
Willian Simon <willian202621@gmail.com>
2026-06-16 18:39:30 -0300

    feito parcialmente tela principal

--- controle-gastos/src/main/java/gui/TelaDeLoginController.java
+++ controle-gastos/src/main/java/gui/TelaDeLoginController.java
@@ -1,12 +1,15 @@
 package gui;
 
+
 import java.io.IOException;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 /*
  classe responsavel por controlar açoes na tela, como quando clicar em login,
@@ -17,8 +20,12 @@ public class TelaDeLoginController {
 	private Button btLogin;
 	@FXML
 	private Button btCriarCOnta;
+	@FXML
+	private TextField email;
+	@FXML
+	private TextField senha;
 	
-	public void onBtLogin() throws IOException {
+	public void onBtCriarConta() throws IOException {
 		 Parent parent = FXMLLoader.load(
 	                getClass().getResource("/gui/TelaCriarConta.fxml"));
 
@@ -28,5 +35,28 @@ public class TelaDeLoginController {
 
 	        stage.setScene(scene);
 	        stage.show();
+	}
+	
+	public void onBtLogin() throws IOException {
+		String emailTxt = email.getText();
+		String senhaTxt = senha.getText();
+		if(!emailTxt.isEmpty() && !senhaTxt.isEmpty()) {
+			Parent parent = FXMLLoader.load(getClass().getResource("/gui/TelaPrincipal.fxml"));
+			
+			Scene scene = new Scene(parent);
+			Stage stage = (Stage) btLogin.getScene().getWindow();
+			
+			stage.setScene(scene);
+			stage.show();
+					
+			
+		}
+		
+		else {
+			System.out.println("Digite email e senha!");
+		}
+		
+		
+		
 	}
 }
